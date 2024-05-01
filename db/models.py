491d3251ae3927c5702d4dac2905add2f7d9e9c1
anirudh-mk <its.me.anirudh.mk@gmail.com>
2024-05-01 18:03:50 +0530

    feat(models created)

--- db/models.py
+++ db/models.py
@@ -1,3 +1,30 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+import uuid
 
-# Create your models here.
+
+class User(AbstractUser):
+    date_joined = None
+
+    id = models.CharField(primary_key=True, default=uuid.uuid4(), max_length=36)
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    username = models.CharField(unique=True, max_length=100)
+    profile_pic = models.ImageField(max_length=200, upload_to='user/', null=True, blank=True)
+    email = models.CharField(unique=True, max_length=200)
+    phone = models.CharField(unique=True, max_length=15)
+    password = models.CharField(max_length=200)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    USERNAME_FIELD = 'email'
+    EMAIL_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    @classmethod
+    def email_exists(cls, email):
+        return cls.objects.filter(email=email).exists()
+
+    class Meta:
+        db_table = 'user'
